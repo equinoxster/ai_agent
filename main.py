6dedd349fcd40708ea7f8d8e2760fd305f61529b
equinoxster <themock@gmail.com>
2025-06-12 19:19:03 +0300

    adds contents as lists

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,8 +15,12 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+
     model = "gemini-2.0-flash-001"
-    contents = sys.argv[1]
+    contents = messages
     response = client.models.generate_content(model=model, contents=contents)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
