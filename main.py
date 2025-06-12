b8f46fcc8708d5d0d298b2f37ab32cd16fa18b38
equinoxster <themock@gmail.com>
2025-06-12 19:13:59 +0300

    reads prompt from command line

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
 
-model = "gemini-2.0-flash-001"
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+def main():
+    if len(sys.argv) < 2 or sys.argv[1] == None:
+        print("Prompt value not found!")
+        sys.exit(1)
 
-response = client.models.generate_content(model=model, contents=contents)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+    contents = sys.argv[1]
+    response = client.models.generate_content(model=model, contents=contents)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+main()
